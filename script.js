c5f1cf1e3301a6e3113b6ca5e8fa25d7d2edab58
DeeshaWellness <148286014+DeeshaWellness@users.noreply.github.com>
2024-04-06 09:48:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
 function submitExercise() {
     var exerciseInput = document.getElementById('exerciseInput').value;
     if (exerciseInput) {
-        // Assuming you have a cloud function URL and a method to generate an ID token
-        var cloudFunctionUrl = 'YOUR_CLOUD_FUNCTION_URL';
-        var idToken = 'YOUR_IDENTITY_TOKEN';
+        // Assuming you have a cloud function URL
+        var cloudFunctionUrl = 'https://us-west3-work-out-sheet-with-shetty.cloudfunctions.net/workoutSheetUpdater';
 
-        fetch(cloudFunctionUrl, {
-            method: 'POST',
-            headers: {
-                'Authorization': 'Bearer ' + idToken,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ exercise: exerciseInput })
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-            return response.json();
-        })
-        .then(data => {
-            document.getElementById('responseMessage').innerText = 'Exercise logged successfully!';
-            console.log(data);
-        })
-        .catch(error => {
-            document.getElementById('responseMessage').innerText = 'Error logging exercise.';
-            console.error('There has been a problem with your fetch operation:', error);
-        });
+        // Check if the user is signed in
+        var googleAuth = gapi.auth2.getAuthInstance();
+        if (googleAuth.isSignedIn.get()) {
+            // Get the ID token of the signed-in user
+            var idToken = googleAuth.currentUser.get().getAuthResponse().id_token;
+
+            // Make the POST request with the exercise data and the ID token
+            fetch(cloudFunctionUrl, {
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer ' + idToken,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ exercise: exerciseInput })
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
+                return response.json();
+            })
+            .then(data => {
+                document.getElementById('responseMessage').innerText = 'Exercise logged successfully!';
+                console.log(data);
+            })
+            .catch(error => {
+                document.getElementById('responseMessage').innerText = 'Error logging exercise.';
+                console.error('There has been a problem with your fetch operation:', error);
+            });
+        } else {
+            document.getElementById('responseMessage').innerText = 'Please sign in to log your exercise.';
+        }
     } else {
         document.getElementById('responseMessage').innerText = 'Please enter an exercise.';
     }
